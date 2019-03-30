98aeea9c11cf1550bd98a34073c5f4ad5480ba74
Litvishko <krooss7777@gmail.com>
2019-03-30 21:35:03 +0200

    Сделал страницу каталога

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,22 @@ var less = require('gulp-less');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
+var minify = require('gulp-csso');
+var rename = require('gulp-rename');
+var svgSprite = require('gulp-svgstore');
+var posthtml = require('gulp-posthtml');
+var include = require('posthtml-include');
+var del = require('del');
 
 gulp.task('less', function () {
   return gulp.src('source/less/styles.less')
     .pipe(less())
     .pipe(postcss([autoprefixer({browsers: ['last 10 version']})
       ]))
-    .pipe(gulp.dest('source/css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(minify())
+    .pipe(rename('style-min.css'))
+    .pipe(gulp.dest('build/css'))
     .pipe(browserSync.reload({
       stream:true
     }));
@@ -18,35 +27,44 @@ gulp.task('less', function () {
 
 gulp.task('serve', function() {
     browserSync.init({
-        server: {
-            baseDir: "./source"
-        }
+        server: "build/"
     });
-    // browserSync.watch('source', browserSync.reload)
+    gulp.watch('source/less/**/*.less', gulp.series('less'));
+    gulp.watch('source/*.html', gulp.series('html')).on('change', browserSync.reload);
 });
 
-gulp.task('watch', function() {
-  gulp.watch('source/less/**/*.less', gulp.series('less'));
+gulp.task('sprite', function() {
+  return gulp.src('source/img/icon-*.svg')
+    .pipe(svgSprite({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
 });
 
-gulp.task('default', gulp.series('less',
-  gulp.parallel('watch','serve')
-  ));
+gulp.task('html', function(done) {
+  return gulp.src('source/*.html')
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('copy', function() {
+  return gulp.src([
+    'source/img/**',
+    'source/fonts/**/*.{woff,woff2}',
+    'source/js/**'
+  ], {
+    base: 'source'
+    })
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function() {
+  return del('build');
+});
 
 
-
-
-
-
-// gulp.task('browser-sync', function() {
-//   browserSync({
-//     server: {
-//       baseDir: 'source'
-//     },
-//     notify: false
-//   });
-// });
-
-// gulp.task('watch', gulp.parallel('browser-sync', 'less'), function() {
-//   gulp.watch('source/less/**/*.less', gulp.parallel('less'));
-// });
+gulp.task('build', gulp.series('clean','copy','less','html'));
+gulp.task('start', gulp.series('build','serve'));
